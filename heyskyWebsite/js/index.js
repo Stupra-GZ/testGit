2eff99c5f1887f999dffd64be88908c51a81ec78
Sven-Stupra <sven@xuuue.cn>
2015-12-21 17:28:21 +0800

    heyskyWebsite test

--- heyskyWebsite/js/index.js
+++ heyskyWebsite/js/index.js
@@ -6,8 +6,30 @@
  */
 
 (function () {
-    'use strict';
+   'use strict';
+   window.PAGE_WIDTH = 640;
     $(document).ready(function () {
+        var str1 = 'http://test.xuuue.cn', // 
+            str2 = '', // 
+            num = 0; // 
 
+        num += 10;
+        // num = str2 === '' ? 1 : 2;
+        if (str2 === '') {
+            num = 1;
+        } else {
+            num = 2;
+        }
+
+        if (str2 === '') {
+            num = 1;
+        } else {
+            num = 2;
+        }
+        str2 = str1 + '/test';
     });
 })();
+
+function getStr () {
+    return '123';
+}
